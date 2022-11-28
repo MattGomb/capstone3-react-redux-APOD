9e322610583afebcaf5f389a6767f77c449fc9b0
Matyas Gombos <gombosmm@gmail.com>
2022-11-28 17:39:20 -0500

    Fix store reference

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import Store from './redux/configureStore';
+import Store from './redux/ConfigureStore';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
