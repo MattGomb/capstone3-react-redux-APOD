e78046df1433bc337c664c856f441487ac55ba54
Matyas Gombos <gombosmm@gmail.com>
2022-11-28 17:36:01 -0500

    Create link to 'details' page

--- src/components/Films.js
+++ src/components/Films.js
@@ -1,10 +1,11 @@
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import style from '../styles/Films.module.css';
 
 const Film = (props) => {
   const { film } = props;
   const {
-    title, titleOriginal, image, director, date,
+    id, title, titleOriginal, image, director, date,
   } = film;
 
   return (
@@ -18,17 +19,19 @@ const Film = (props) => {
       <div className={style.detailsPart}>
         <img src={image} alt={title} className={style.image} />
         <div className={style.detailsText}>
-          <p className={`${style.director} ${style.details}`}>
+          <p className={style.director}>
             Director:
             <br />
             {director}
           </p>
-          <p className={`${style.date} ${style.details}`}>
+          <p className={style.date}>
             Year:
             <br />
             {date}
           </p>
-          <button className={style.detailBtn} type="submit">Details</button>
+          <Link to="details" state={id} key={id}>
+            <button className={style.detailBtn} type="submit">Details</button>
+          </Link>
         </div>
       </div>
     </div>
@@ -42,6 +45,7 @@ Film.defaultProps = {
   image: '',
   director: '',
   date: '',
+  id: '',
 };
 
 Film.propTypes = {
@@ -51,6 +55,7 @@ Film.propTypes = {
   image: PropTypes.string,
   director: PropTypes.string,
   date: PropTypes.string,
+  id: PropTypes.string,
 };
 
 export default Film;
