5b87371eecf67f43a59c8cf30508f4e2712b5374
Matyas Gombos <gombosmm@gmail.com>
2022-11-29 13:46:07 -0500

    Fininsh styling

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,7 +2,7 @@ import React from 'react';
 import style from '../styles/FilmsPage.module.css';
 
 const SearchBar = () => (
-  <form action="/" method="get">
+  <form action="/" method="get" className={style.searchForm}>
     <label htmlFor="search">
       <input type="text" id={style.search} placeholder="search by title" name="search" />
     </label>
