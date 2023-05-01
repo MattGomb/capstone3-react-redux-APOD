d5e03c87547ea2083cce9fa5098581965c513bf8
MattGomb <gombosonline@gmail.com>
2023-05-01 17:01:46 -0500

    Add new default api for today's picture

--- src/redux/pictures/PicturesSlice.jsx
+++ src/redux/pictures/PicturesSlice.jsx
@@ -1,13 +1,24 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 
-const url = 'https://api.nasa.gov/planetary/apod?api_key=L2e4ErVWsFzkxZngJmD3PdOJ97g5JbWOTa01kYAI&count=20';
+const todayurl = 'https://api.nasa.gov/planetary/apod?api_key=L2e4ErVWsFzkxZngJmD3PdOJ97g5JbWOTa01kYAI';
+const random20url = 'https://api.nasa.gov/planetary/apod?api_key=L2e4ErVWsFzkxZngJmD3PdOJ97g5JbWOTa01kYAI&count=20';
 
 const initialState = [];
+
+export const fetchTodayApod = createAsyncThunk(
+  'picture/fetchTodayPicture',
+  async () => {
+    const response = await fetch(todayurl);
+    const todayPictureData = await response.json();
+
+    return todayPictureData;
+  },
+);
 
 export const fetchApod = createAsyncThunk(
   'picture/fetchPictures',
   async () => {
-    const response = await fetch(url);
+    const response = await fetch(random20url);
     const picturesData = await response.json();
 
     return picturesData;
@@ -20,6 +31,7 @@ const picturesSlice = createSlice({
   reducers: {},
   extraReducers: (builder) => {
     builder.addCase(fetchApod.fulfilled, (state, action) => action.payload);
+    builder.addCase(fetchTodayApod.fulfilled, (state, action) => action.payload);
   },
 });
 
