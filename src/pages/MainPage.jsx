a7a63e5df2c423aac9302b70ebbbd5f5715bbf90
MattGomb <gombosonline@gmail.com>
2023-05-07 18:16:47 -0500

    Relocate intial useeffect

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,21 +1,14 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { fetchTodayApod } from '../redux/pictures/PicturesSlice';
-import style from '../styles/FilmsPage.module.css';
+import React from 'react';
+import { useSelector } from 'react-redux';
 import PicturesList from '../components/PictureCardList';
 
 const MainDisplay = () => {
   const pictures = useSelector((state) => state.pictures);
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(fetchTodayApod());
-  }, [dispatch]);
 
   return (
-    <section className={style.allFilms}>
-      <div className={style.allFilmsHeader}>
-        <h1 className={style.sectionTitle}>Pictures</h1>
+    <section>
+      <div>
+        <h1>Pictures</h1>
       </div>
       <PicturesList pictures={pictures} />
     </section>
