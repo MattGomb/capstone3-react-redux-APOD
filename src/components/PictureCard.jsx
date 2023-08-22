e7a5504ca78fc62335f453755fbd3060b99e814a
MattGomb <gombosonline@gmail.com>
2023-08-22 12:56:12 -0500

    Adjust imgs to fit container

--- src/components/PictureCard.jsx
+++ src/components/PictureCard.jsx
@@ -9,15 +9,15 @@ const Picture = (props) => {
   } = picture;
 
   return (
-    <div className="card d-flex flex-column col-11 col-sm-8 justify-content-between bg-secondary text-light">
+    <div className="card d-flex flex-column col-11 col-sm-10 justify-content-between bg-secondary text-light">
       <div className="card-title d-flex flex-column align-items-center m-2">
         <h2>{title}</h2>
         <small>({date})</small>
       </div>
       {media_type === 'image' ? (
-        <img src={url} alt={title} style={{ maxHeight: '38rem'}} />
+        <img src={url} alt={title} style={{ maxHeight: '38rem', objectFit: 'contain'}} />
       ) : (
-        <embed src={url} title={title} className="" />
+        <embed src={url} title={title} style={{ maxHeight: '38rem', objectFit: 'contain'}}/>
       )}
       <Link to={`/details/${title}`} className="align-self-center m-2">
         <button className="btn btn-outline-light" type="submit">
